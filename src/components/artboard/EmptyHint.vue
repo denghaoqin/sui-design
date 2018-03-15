a8bfa3e833f089468b583fe8c9a39746ea0c9bfd
leomYili <leomy@outlook.com>
2018-03-15 17:50:48 +0800

    框架组件错误

---
+++ src/components/artboard/EmptyHint.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="empty-hint">
+        请新建模块
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+    .empty-hint{
+        height:100%;
+        width:100%;
+        border:1px dashed #5873d8;
+        text-align: center;
+        padding-top: 50%;
+    }
+</style>
--- src/components/artboard/PhoneArtboard.vue
+++ src/components/artboard/PhoneArtboard.vue
@@ -2,28 +2,127 @@
     <div class="phone-artboard">
         <div class="main" :style="{background:`url(${phoneBg}) no-repeat 0 0`,backgroundSize:'contain'}">
             <div class="phone-view">
-                这是设计页面
+                <div v-for="(list, zone) in dropzones" class="v-col--40" :key="zone">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3>Dropzone {{zone}}</h3>
+                        </div>
+                        <div class="panel-body">
+                            <vddl-list class="panel__body--list" :list="list" :inserted="inserted" effect-allowed="move" :external-sources="true">
+                                <list v-for="(item, index) in list" :key="item.id" :item="item" :list="list" :index="index">
+                                </list>
+                                <vddl-placeholder class="red">Custom placeholder</vddl-placeholder>
+                            </vddl-list>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-
 <script>
 import phoneBg from "../../assets/images/phone-bg.png";
-import 'element-ui/lib/theme-chalk/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "element-ui/lib/theme-chalk/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import EmptyHint from "./EmptyHint";
+import List from "./List";
 
 export default {
     name: "phone-artboard",
+    components: {
+        EmptyHint,
+        List
+    },
     data() {
         return {
-            phoneBg: phoneBg
+            phoneBg: phoneBg,
+            dropzones: {
+                A: [
+                    {
+                        type: "container",
+                        id: 1,
+                        columns: [
+                            {
+                                type: "item",
+                                id: "1"
+                            },
+                            {
+                                type: "item",
+                                id: "2"
+                            },
+                            {
+                                type: "item",
+                                id: "3"
+                            }
+                        ]
+                    },
+                    {
+                        type: "item",
+                        id: "4"
+                    },
+                    {
+                        type: "item",
+                        id: "5"
+                    },
+                    {
+                        type: "item",
+                        id: "6"
+                    }
+                ],
+                B: [
+                    {
+                        type: "item",
+                        id: 7
+                    },
+                    {
+                        type: "item",
+                        id: "8"
+                    },
+                    {
+                        type: "container",
+                        id: "2",
+                        columns: [
+                            {
+                                type: "item",
+                                id: "9"
+                            },
+                            {
+                                type: "item",
+                                id: "10"
+                            },
+                            {
+                                type: "container",
+                                id: "3",
+                                columns: [
+                                    {
+                                        type: "item",
+                                        id: "13"
+                                    },
+                                    {
+                                        type: "item",
+                                        id: "14"
+                                    }
+                                ]
+                            },
+                            {
+                                type: "item",
+                                id: "15"
+                            }
+                        ]
+                    }
+                ]
+            }
         };
     },
-    computed:{
-        iframeSrc:function(){
-            return 'http://'+window.location.host+window.location.pathname+"#/phone-design"
+    computed: {
+        isEmpty: function() {
+            return this.list.length === 0;
+        }
+    },
+    methods: {
+        inserted: function(data) {
+            console.log(data);
         }
     }
 };
@@ -33,7 +132,7 @@ export default {
 .phone-artboard {
     padding-top: 20px;
     padding-bottom: 20px;
-    flex:1;
+    flex: 1;
 
     .main {
         width: 292px;
@@ -41,7 +140,7 @@ export default {
         z-index: 1;
         position: relative;
         height: 600px;
-        padding:74px 22px 82px 22px;
+        padding: 74px 22px 82px 22px;
 
         .phone-view {
             width: 100%;
@@ -49,6 +148,23 @@ export default {
             border: 0;
             overflow-x: hidden;
             overflow-y: auto;
+
+            .drop-container {
+                width: 100%;
+                height: 100%;
+                border: 0;
+                overflow-x: hidden;
+                overflow-y: auto;
+
+                .vddl-placeholder {
+                    height: 30px;
+                    background-color: #ccc;
+                }
+
+                .vddl-dragging-source {
+                    display: none;
+                }
+            }
         }
     }
 }
