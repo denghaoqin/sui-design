480bf850ec6bf2dcd371ab4db02130beda0376f4
love-m <1418772902@qq.com>
2018-03-16 15:10:45 +0800

    多列排版完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <freecontent width="80%" isempty="true" titleicon="test" showborder="true" showtitle="false" bordercolor="blue"/>
-    <columnlayout isempty="true" showborder="true" showtitle="true" contentwidth="1400" bordercolor="blue"/>
+    <freecontent width="80%" isempty="true" titleicon="test" showtitle="false" height="300" titleheight="0"/>
+    <multiseriatelayout width="80%" height="300" titleheight="48" isempty="true" showtitle="true" freecontentnum="3" freecontentspace="10"/>
+    <columnlayout isempty="true" contentwidth="1400"  height="300" titleheight="0"/>
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import Freecontent from '@/components/layout/Freecontent'
 import Columnlayout from '@/components/layout/Columnlayout'
+import Multiseriatelayout from '@/components/layout/Multiseriatelayout'
 export default {
   name: 'home',
   components: {
     Freecontent,
-    Columnlayout
+    Columnlayout,
+    Multiseriatelayout
   }
 }
 </script>
